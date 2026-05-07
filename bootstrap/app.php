bde8bb999443bc3d15f00d3a7cb4a53650c07858
Roger Jeremy <rogerjeremy03@gmail.com>
2026-05-07 14:27:11 +0700

    Fix Target class view does not exist

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,7 @@ if (isset($_ENV['VERCEL']) || getenv('VERCEL')) {
     }
 }
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -25,3 +25,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+if (isset($_ENV['VERCEL']) || getenv('VERCEL')) {
+    $app->useStoragePath('/tmp/storage');
+}
+
+return $app;
